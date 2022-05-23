366903a7b22de247c1b4d087607ce3b1acc2b267
Uche Okereke <orezpablo@gmail.com>
2022-05-23 14:16:15 +0200

    Second commit

---
+++ client/src/setupProxy.js
@@ -0,0 +1,40 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware("/api1", {
+      target: "http://localhost:9000", // API endpoint 1
+      changeOrigin: true,
+      pathRewrite: {
+        "^/api1": "",
+      },
+      headers: {
+        Connection: "keep-alive",
+      },
+    })
+  );
+  app.use(
+    createProxyMiddleware("/api2", {
+      target: "http://localhost:9001", // API endpoint 2
+      changeOrigin: true,
+      pathRewrite: {
+        "^/api2": "",
+      },
+      headers: {
+        Connection: "keep-alive",
+      },
+    })
+  );
+  app.use(
+    createProxyMiddleware("/api3", {
+      target: "http://localhost:9002", // API endpoint 3
+      changeOrigin: true,
+      pathRewrite: {
+        "^/api3": "",
+      },
+      headers: {
+        Connection: "keep-alive",
+      },
+    })
+  );
+};
